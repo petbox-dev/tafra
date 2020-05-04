e73aa18472354594136576f584a840918b22fd15
Derrick W. Turk <dwt@terminusdatascience.com>
2020-05-04 11:38:37 -0500

    Move DataFrame stub into separate stub file.

--- tafra.py
+++ tafra.py
@@ -17,6 +17,7 @@ from itertools import chain
 import dataclasses as dc
 
 import numpy as np
+from pandas import DataFrame # just for mypy...
 
 from typing import Any, Callable, Dict, List, Iterable, Tuple, Optional, Union
 from typing import cast
@@ -66,20 +67,6 @@ def _real_has_attribute(obj: object, attr: str) -> bool:
         return True
     except AttributeError:
         return False
-
-
-class DataFrame(Protocol):
-    """A fake class to satisfy typing of a `pandas.DataFrame` without a dependency.
-    """
-    _data: Dict[str, np.ndarray]
-    columns: List[str]
-    dtypes: List[str]
-
-    def __getitem__(self, column: str) -> np.ndarray:
-        ...
-
-    def __setitem__(self, column: str, value: np.ndarray):
-        ...
 
 
 @dc.dataclass
