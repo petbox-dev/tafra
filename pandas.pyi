49fad84c5c407f519f80d6e78bec2b56f8af0778
David Fulford <dfulford@gryphonog.com>
2020-05-04 20:20:19 -0500

    does this join even work?

--- pandas.pyi
+++ pandas.pyi
@@ -9,7 +9,7 @@ class DataFrame():
     dtypes: List[str]
 
     def __getitem__(self, column: str) -> np.ndarray:
-        return self._data[column]
+        ...
 
     def __setitem__(self, column: str, value: np.ndarray):
-        self._data[column] = value
+        ...
